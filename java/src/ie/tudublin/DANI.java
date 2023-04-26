0576a5682b2d16688d7d793de4e8da1cc2c5e954
JKearns3 <C21383126@mytudublin.ie>
2023-04-26 18:23:00 +0100

    get follows count

--- java/src/ie/tudublin/DANI.java
+++ java/src/ie/tudublin/DANI.java
@@ -7,6 +7,7 @@ import processing.core.PApplet;
 public class DANI extends PApplet {
 
 	ArrayList<Follow> follows = new ArrayList<Follow>();
+	ArrayList<Word> wordsList = new ArrayList<Word>();
 
 
 	public void settings() {
@@ -48,10 +49,10 @@ public class DANI extends PApplet {
 	public void loadFile()
 	{
 		String[] lines = loadStrings("small.txt"); // array of lines from the txt file
-		String allWords = " "; //a long string of all the words from the txt file
+		String allWords = ""; //a long string of all the words from the txt file
 		for(String line: lines) //add all the lines to words
 		{
-			allWords += " " + line;
+			allWords += line + " " ;
 	
 		}
 
@@ -66,17 +67,17 @@ public class DANI extends PApplet {
 		// go through each word and note the word after it
 			// check through all the words after for that combination
 			// do findWord in case the following count was done already??
+		// add follows array list to Word class
 
-		
 		for(int i = 0; i < words.length - 1; i++)
 		{
-			int count = 0;
+			int count = 1;
 			String s = words[i];
 			String next = words[i+1];
-
+	
 			for(int j = i+1; j < words.length-1; j++)
 			{
-				if(words[j] == s && words[j+1] == next)
+				if(words[j].equals(s) && words[j+1].equals(next))
 				{
 					count ++;
 				}
@@ -84,10 +85,27 @@ public class DANI extends PApplet {
 
 			Follow f = new Follow(next, count);
 			follows.add(f);
-			System.out.println(f);
+			//System.out.println(f);		
 
 		}
-		
+
+		for(int i = 0; i <= follows.size() - 1; i++)
+		{
+			for(int j = 1; j <= words.length -1 ; i++)
+			{
+				if(follows.get(i).getWord() == words[j] )
+				{
+					Word w = new Word(words[j-1], follows);
+					wordsList.add(w);
+				}
+			}
+			
+		}
+		for( Word w : wordsList)
+		{
+			System.out.println(w);
+		}
+				
 	
 		
 	}
